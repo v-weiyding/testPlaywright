0e9ad9b58a49bde6394ce63437e78e5f0a655500
v-weiyding <v-weiyding@microsoft.com>
2024-09-10 12:10:43 +0800

    1.txt

---
+++ 1.js
@@ -0,0 +1,35 @@
+const { webkit } = require('playwright');  // Or 'chromium' or 'firefox'.
+const fs = require('fs');
+
+
+(async () => {
+    const browser = await webkit.launch();
+    const context = await browser.newContext();
+    const page = await context.newPage();
+    await page.goto('https://example.com');
+    //   await page.screenshot({ path: 'screenshot.png' });
+    let text = await page.locator("html").innerText()
+    console.log(text);
+
+
+    // 写入文件的路径和文件名
+    const filePath = './1.txt';
+
+    // 使用fs.writeFile方法写入文件
+    fs.writeFile(filePath, text, (err) => {
+        if (err) {
+            console.error('写入文件时发生错误:', err);
+        } else {
+            console.log('文件写入成功！');
+        }
+    });
+
+
+    await browser.close();
+    console.log(111111111111111111);
+})();
+
+
+
+
+
